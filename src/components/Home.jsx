0b872feafa03ee489f7d233deeabf95abee63a46
furqan_ashraf <furqan_ashraf@outlook.com>
2021-06-29 02:24:25 +0500

    modified footer and home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -11,11 +12,14 @@ const Home = () => {
           class="ui text container"
           style={{ paddingBottom: "12%", paddingTop: "12%" }}
         >
-          <h1 class="ui inverted header">Imagine-a-Company</h1>
-          <h2>Do whatever you want when you want to.</h2>
-          <div class="ui huge primary button">
-            Get Started <i class="right arrow icon"></i>
-          </div>
+          <h1 class="ui inverted header">Feeling Insecure ?</h1>
+          <h2>Transfer your money more securely and fast.</h2>
+          <h1>COINCHAIN</h1>
+          <Link to="/signup">
+            <div class="ui huge primary button">
+              Get Started <i class="right arrow icon"></i>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
